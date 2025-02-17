ea4dbc30c7b19db3cb8035fa4f102644f5db3696
PIUNI <125922996+chenfeng32@users.noreply.github.com>
2025-02-17 11:52:11 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,13 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-<<<<<<< Updated upstream
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-=======
 pub const RENDEZVOUS_SERVERS: &[&str] = &["47.76.82.81"];
 pub const RS_PUB_KEY: &str = "KrUEnBz1gIY4MaZ4yNnIddCn9sLvrH8BL1dC+KQm6EE=";
->>>>>>> Stashed changes
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
